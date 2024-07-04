42fa4c543a5a37b5c6e90e6c56a64bf62494bb49
Juani-tech <jperezdichiazza@gmail.com>
2024-07-04 14:24:13 -0300

    add beforeEach

--- e2e_testing/tests/note_app.spec.js
+++ e2e_testing/tests/note_app.spec.js
@@ -1,27 +1,27 @@
-const { test, expect } = require("@playwright/test");
+const { test, expect } = require('@playwright/test')
 
-test.describe("Note app", () => {
-  test("front page can be opened", async ({ page }) => {
-    await page.goto("http://localhost:5173");
+test.describe('Note app', () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://localhost:5173')
+  })
 
-    const locator = await page.getByText("Notes");
-    await expect(locator).toBeVisible();
+  test('front page can be opened', async ({ page }) => {
+    const locator = await page.getByText('Notes')
+    await expect(locator).toBeVisible()
     await expect(
       page.getByText(
-        "Note app, Department of Computer Science, University of Helsinki 2024"
-      )
-    ).toBeVisible();
-  });
+        'Note app, Department of Computer Science, University of Helsinki 2023',
+      ),
+    ).toBeVisible()
+  })
 
-  test("login form can be opened", async ({ page }) => {
-    await page.goto("http://localhost:5173");
-
-    await page.getByRole("button", { name: "log in" }).click();
-    await page.getByRole("textbox").first().fill("root");
-    await page.getByRole("textbox").last().fill("sekret");
-    await page.getByRole("button", { name: "login" }).click();
+  test('login form can be opened', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByRole('textbox').first().fill('root')
+    await page.getByRole('textbox').last().fill('sekret')
+    await page.getByRole('button', { name: 'login' }).click()
 
     // Me olvide de ponerle user.name, por lo que printea 'vacio'
-    await expect(page.getByText("logged-in")).toBeVisible();
-  });
-});
+    await expect(page.getByText('logged-in')).toBeVisible()
+  })
+})
