0562f20839ed225901ac056d70ac4b165f8934c5
Juani-tech <jperezdichiazza@gmail.com>
2024-07-04 18:07:24 -0300

    5.2

--- e2e_testing/tests/note_app.spec.js
+++ e2e_testing/tests/note_app.spec.js
@@ -1,9 +1,10 @@
 const { test, describe, expect, beforeEach } = require('@playwright/test')
+const { loginWith, createNote } = require('./helper')
 
 describe('Note app', () => {
   beforeEach(async ({ page, request }) => {
-    await request.post('http:localhost:3001/api/testing/reset')
-    await request.post('http://localhost:3001/api/users', {
+    await request.post('/api/testing/reset')
+    await request.post('/api/users', {
       data: {
         name: 'Juani',
         username: 'juani',
@@ -11,7 +12,7 @@ describe('Note app', () => {
       },
     })
 
-    await page.goto('http://localhost:5173')
+    await page.goto('/')
   })
 
   test('front page can be opened', async ({ page }) => {
@@ -25,35 +26,42 @@ describe('Note app', () => {
   })
 
   test('login form can be opened', async ({ page }) => {
-    await page.getByRole('button', { name: 'log in' }).click()
-    await page.getByTestId('username').fill('juani')
-    await page.getByTestId('password').fill('pdc')
-    await page.getByRole('button', { name: 'login' }).click()
+    await loginWith(page, 'juani', 'pdc')
 
     // Me olvide de ponerle user.name, por lo que printea 'vacio'
     await expect(page.getByText('Juani logged-in')).toBeVisible()
   })
 
+  test('login fails with wrong password', async ({ page }) => {
+    await page.getByRole('button', { name: 'log in' }).click()
+    await page.getByTestId('username').fill('juani')
+    await page.getByTestId('password').fill('wrong')
+    await page.getByRole('button', { name: 'login' }).click()
+
+    // Esto es para asegurar de que el error aparece en el lugar correcto (.error)
+    const errorDiv = page.locator('.error')
+    await expect(errorDiv).toContainText('Wrong credentials')
+    await expect(errorDiv).toHaveCSS('border-style', 'solid')
+    await expect(errorDiv).toHaveCSS('color', 'rgb(255, 0, 0)')
+    await expect(page.getByText('Juani logged in')).not.toBeVisible()
+  })
+
   describe('when logged in', () => {
     beforeEach(async ({ page }) => {
-      await page.getByRole('button', { name: 'log in' }).click()
-      await page.getByTestId('username').fill('juani')
-      await page.getByTestId('password').fill('pdc')
-      await page.getByRole('button', { name: 'login' }).click()
+      await loginWith(page, 'juani', 'pdc')
     })
 
     test('a new note can be created', async ({ page }) => {
-      await page.getByRole('button', { name: 'new note' }).click()
-      await page.getByRole('textbox').fill('a note created by playwright')
-      await page.getByRole('button', { name: 'save' }).click()
+      await createNote(page, 'a note created by playwright', true)
+
       await expect(page.getByText('a note created by playwright')).toBeVisible()
     })
 
     describe('and a note exists', () => {
       beforeEach(async ({ page }) => {
-        await page.getByRole('button', { name: 'new note' }).click()
-        await page.getByRole('textbox').fill('another note by playwright')
-        await page.getByRole('button', { name: 'save' }).click()
+        // await page.getByRole('button', { name: 'new note' }).click()
+        // await page.getByRole('textbox').fill('another note by playwright')
+        await createNote(page, 'another note by playwright', true)
       })
 
       test('importance can be changed', async ({ page }) => {
